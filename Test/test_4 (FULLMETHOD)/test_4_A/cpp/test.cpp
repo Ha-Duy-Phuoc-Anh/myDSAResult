78cc18d5c0ac4cfdf34910d9412f0ea19c1791cb
Phixah <nicknameabc4@gmail.com>
2025-07-20 04:36:50 +0000

    new file:   Test/test_4 (FULLMETHOD)/test_4_A/Makefile 	new file:   Test/test_4 (FULLMETHOD)/test_4_A/cpp/test.cpp 	new file:   Test/test_4 (FULLMETHOD)/test_4_A/test.py 	new file:   Test/test_4 (FULLMETHOD)/test_4_B/cpp/test.cpp 	new file:   Test/test_4 (FULLMETHOD)/test_4_B/test.py 	new file:   Test/test_4 (FULLMETHOD)/test_4_C/cpp/test.cpp 	new file:   Test/test_4 (FULLMETHOD)/test_4_C/test.py

---
+++ Test/test_4 (FULLMETHOD)/test_4_A/cpp/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class LinkedList {
+private:
+    // Create node for
+    struct Node {
+        int data;
+        Node* next;
+        Node(int val) : data(val), next(nullptr) {};
+    };
+};
